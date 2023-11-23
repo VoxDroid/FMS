45b1bcc10a3b9861a6aa992d7d5fa5df27a65b39
VoxDroid <jandreijandrei103@gmail.com>
2023-11-23 18:40:17 +0800

    Export Front

--- Pages/Export.Designer.cs
+++ Pages/Export.Designer.cs
@@ -28,7 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Utilities.BunifuPages.BunifuAnimatorNS.Animation animation1 = new Utilities.BunifuPages.BunifuAnimatorNS.Animation();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Export));
             this.crebud = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2ComboBox2 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.bunifuPages1 = new Bunifu.UI.WinForms.BunifuPages();
+            this.select = new System.Windows.Forms.TabPage();
+            this.budman = new System.Windows.Forms.TabPage();
+            this.tranlo = new System.Windows.Forms.TabPage();
+            this.bunifuPages1.SuspendLayout();
             this.SuspendLayout();
             // 
             // crebud
@@ -42,14 +53,155 @@
             this.crebud.TabIndex = 37;
             this.crebud.Text = "Export Data";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 15F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 36);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "Select Data to Export:";
+            // 
+            // guna2ComboBox2
+            // 
+            this.guna2ComboBox2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox2.BorderRadius = 5;
+            this.guna2ComboBox2.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox2.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox2.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox2.Font = new System.Drawing.Font("Poppins", 10F);
+            this.guna2ComboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.guna2ComboBox2.ItemHeight = 30;
+            this.guna2ComboBox2.Items.AddRange(new object[] {
+            "-- Select --",
+            "Budget Management Data",
+            "Transaction Logs Data"});
+            this.guna2ComboBox2.Location = new System.Drawing.Point(21, 117);
+            this.guna2ComboBox2.Name = "guna2ComboBox2";
+            this.guna2ComboBox2.Size = new System.Drawing.Size(228, 36);
+            this.guna2ComboBox2.StartIndex = 0;
+            this.guna2ComboBox2.TabIndex = 40;
+            this.guna2ComboBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 15F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(15, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 36);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Export Format:";
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.BorderRadius = 5;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Poppins", 10F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.Items.AddRange(new object[] {
+            "-- Select --",
+            "CSV",
+            "Excel",
+            "PDF"});
+            this.guna2ComboBox1.Location = new System.Drawing.Point(21, 212);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.Size = new System.Drawing.Size(228, 36);
+            this.guna2ComboBox1.StartIndex = 0;
+            this.guna2ComboBox1.TabIndex = 42;
+            this.guna2ComboBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bunifuPages1
+            // 
+            this.bunifuPages1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.bunifuPages1.AllowTransitions = true;
+            this.bunifuPages1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bunifuPages1.Controls.Add(this.select);
+            this.bunifuPages1.Controls.Add(this.budman);
+            this.bunifuPages1.Controls.Add(this.tranlo);
+            this.bunifuPages1.Location = new System.Drawing.Point(275, 13);
+            this.bunifuPages1.Multiline = true;
+            this.bunifuPages1.Name = "bunifuPages1";
+            this.bunifuPages1.Page = this.select;
+            this.bunifuPages1.PageIndex = 0;
+            this.bunifuPages1.PageName = "select";
+            this.bunifuPages1.PageTitle = "Select";
+            this.bunifuPages1.SelectedIndex = 0;
+            this.bunifuPages1.Size = new System.Drawing.Size(460, 475);
+            this.bunifuPages1.TabIndex = 43;
+            animation1.AnimateOnlyDifferences = false;
+            animation1.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.BlindCoeff")));
+            animation1.LeafCoeff = 0F;
+            animation1.MaxTime = 1F;
+            animation1.MinTime = 0F;
+            animation1.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicCoeff")));
+            animation1.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicShift")));
+            animation1.MosaicSize = 0;
+            animation1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            animation1.RotateCoeff = 0F;
+            animation1.RotateLimit = 0F;
+            animation1.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.ScaleCoeff")));
+            animation1.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.SlideCoeff")));
+            animation1.TimeCoeff = 0F;
+            animation1.TransparencyCoeff = 0F;
+            this.bunifuPages1.Transition = animation1;
+            this.bunifuPages1.TransitionType = Utilities.BunifuPages.BunifuAnimatorNS.AnimationType.Custom;
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(4, 4);
+            this.select.Name = "select";
+            this.select.Padding = new System.Windows.Forms.Padding(3);
+            this.select.Size = new System.Drawing.Size(452, 449);
+            this.select.TabIndex = 0;
+            this.select.Text = "Select";
+            this.select.UseVisualStyleBackColor = true;
+            // 
+            // budman
+            // 
+            this.budman.Location = new System.Drawing.Point(4, 4);
+            this.budman.Name = "budman";
+            this.budman.Padding = new System.Windows.Forms.Padding(3);
+            this.budman.Size = new System.Drawing.Size(452, 449);
+            this.budman.TabIndex = 1;
+            this.budman.Text = "Budget Management";
+            this.budman.UseVisualStyleBackColor = true;
+            // 
+            // tranlo
+            // 
+            this.tranlo.Location = new System.Drawing.Point(4, 4);
+            this.tranlo.Name = "tranlo";
+            this.tranlo.Padding = new System.Windows.Forms.Padding(3);
+            this.tranlo.Size = new System.Drawing.Size(452, 449);
+            this.tranlo.TabIndex = 2;
+            this.tranlo.Text = "Transaction Logs";
+            this.tranlo.UseVisualStyleBackColor = true;
+            // 
             // Export
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.Controls.Add(this.bunifuPages1);
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.guna2ComboBox2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.crebud);
             this.Name = "Export";
             this.Size = new System.Drawing.Size(750, 500);
+            this.bunifuPages1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +210,13 @@
         #endregion
 
         private System.Windows.Forms.Label crebud;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox2;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private Bunifu.UI.WinForms.BunifuPages bunifuPages1;
+        private System.Windows.Forms.TabPage select;
+        private System.Windows.Forms.TabPage budman;
+        private System.Windows.Forms.TabPage tranlo;
     }
 }
